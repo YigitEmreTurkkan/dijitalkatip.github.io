5b2ad73c9a5a0ead0ab3aa564f14937b3216c2b5
Yigit Emre Turkkan <yigit.turkkan@cloudflex.com.tr>
2026-01-02 16:20:05 +0300

    bug fix #3

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // Bu repo bir "project page" olarak yayındaysa
 // nihai URL: https://yigitemreturkkan.github.io/dijitalkatip.github.io/
@@ -9,12 +14,9 @@ export default defineConfig({
   base: "/dijitalkatip.github.io/",
   resolve: {
     alias: {
-      "pdfmake/build/pdfmake": "pdfmake/build/pdfmake.js",
-      "pdfmake/build/vfs_fonts": "pdfmake/build/vfs_fonts.js"
+      "pdfmake/build/pdfmake": resolve(__dirname, "node_modules/pdfmake/build/pdfmake.js"),
+      "pdfmake/build/vfs_fonts": resolve(__dirname, "node_modules/pdfmake/build/vfs_fonts.js")
     }
-  },
-  optimizeDeps: {
-    include: ["pdfmake", "pdfmake/build/pdfmake.js", "pdfmake/build/vfs_fonts.js"]
   }
 });
 
